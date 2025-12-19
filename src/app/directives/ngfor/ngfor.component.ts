746c1fa5d52177468dfa510aacdd28dd6b33ee85
dilayuluerguven <uluerguven245@gmail.com>
2025-12-19 16:09:35 +0300

    Implement ngFor directive for list rendering

--- src/app/directives/ngfor/ngfor.component.ts
+++ src/app/directives/ngfor/ngfor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-ngfor',
@@ -8,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class NgforComponent implements OnInit {
 
   names:string[] = ['Mehmet', 'Ayşe', 'Fatma'];
-  constructor() { }
+  userList:User[] = [];
+  constructor() {
+    this.userList.push(new User(1,'Mehmet','mehmet@example.com'));
+    this.userList.push(new User(2,'Ahmet','ahmet@example.com'));
+    this.userList.push(new User(3,'Ayşe','ayse@example.com'));
+    this.userList.push(new User(4,'Fatma','fatma@example.com'));
+   }
   ngOnInit(): void {
   }
   
