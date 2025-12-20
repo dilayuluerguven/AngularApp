f90815c4048450ee8c0e7da05fefef7e00903f1d
dilayuluerguven <uluerguven245@gmail.com>
2025-12-20 23:59:03 +0300

    add json pipe to template

--- src/app/pipes/pipe/pipe.component.ts
+++ src/app/pipes/pipe/pipe.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pipe',
   templateUrl: './pipe.component.html',
   styleUrls: ['./pipe.component.css']
 })
-export class PipeComponent {
+export class PipeComponent implements OnInit {
 
+  person1:Person={firstName:'John', lastName:'Doe', age:30};
+  person2:any={first:'Jane', last:'Smith', age:25};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
 }
