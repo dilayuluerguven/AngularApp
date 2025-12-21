a19c62829d68c26a597a70db43c61dcd8cae5a42
dilayuluerguven <uluerguven245@gmail.com>
2025-12-21 17:44:19 +0300

    Reactive form validation logic and reusable control state helpers added

--- src/app/forms/reactive-type2/reactive-type2.component.ts
+++ src/app/forms/reactive-type2/reactive-type2.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CountryDropMenu } from 'src/app/models/country-drop-menu';
+import { GenderRadioMenu } from 'src/app/models/gender-radio-menu';
 
 @Component({
   selector: 'app-reactive-type2',
@@ -9,14 +11,34 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class ReactiveType2Component implements OnInit {
 
   loginForm:FormGroup;
+  countryMenu: CountryDropMenu[] = [
+      { text: 'Seçiniz', value: 0 },
+      { text: 'Türkiye', value: 1 },
+      { text: 'Almanya', value: 2 },
+      { text: 'Fransa', value: 3 },
+      { text: 'Hollanda', value: 4 },
+    ];
+    genderMenu: GenderRadioMenu[] = [
+        { text: 'Erkek', value: 1 },
+        { text: 'Kadın', value: 2 },
+      ];
   constructor() {
     this.loginForm=new FormGroup({
-      userName: new FormControl(),
-      password: new FormControl()
+      userName: new FormControl('',Validators.required),
+      password: new FormControl('',[  Validators.required,Validators.minLength(6)]),
+      rememberMe:new FormControl(),
+      country:new FormControl(0),
+      gender:new FormControl(1)
     });
   }
   ngOnInit(): void {}
   signIn() {
     console.log(this.loginForm.value);
+  }
+  isInValidMessageShow(formControl:string)
+  {
+    return this.loginForm.get(formControl)?.invalid &&
+          (this.loginForm.get(formControl)?.dirty || this.loginForm.get(formControl)?.touched)
+
   }
 }
--- src/app/forms/reactive/reactive.component.ts
+++ src/app/forms/reactive/reactive.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CountryDropMenu } from 'src/app/models/country-drop-menu';
+import { GenderRadioMenu } from 'src/app/models/gender-radio-menu';
 
 @Component({
   selector: 'app-reactive',
@@ -8,14 +10,46 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ReactiveComponent implements OnInit {
   loginForm: FormGroup;
+  countryMenu: CountryDropMenu[] = [
+    { text: 'Türkiye', value: 1 },
+    { text: 'Almanya', value: 2 },
+    { text: 'Fransa', value: 3 },
+    { text: 'Hollanda', value: 4 },
+  ];
+  genderMenu: GenderRadioMenu[] = [
+    { text: 'Erkek', value: 1 },
+    { text: 'Kadın', value: 2 },
+  ];
   constructor(fb: FormBuilder) {
     this.loginForm = fb.group({
-      userName: '',//FormControl()
-      password: '',
+      userName: fb.control('', Validators.required), //FormControl()
+      password: fb.control('', [Validators.required, Validators.minLength(6)]),
+      rememberMe: false,
+      country: fb.control('',Validators.required),
+      gender: [1],
     });
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.loginForm.get("userName")?.valid
+  }
   signIn() {
     console.log(this.loginForm.value);
   }
+  isInValidMessageShow(formControl:string)
+  {
+    return this.loginForm.get(formControl)?.invalid &&
+          (this.loginForm.get(formControl)?.dirty || this.loginForm.get(formControl)?.touched)
+
+  }
+  isValid(formControlName: string): boolean {
+    let formControl = this.loginForm.get(formControlName);
+      if (!(formControl?.invalid && (formControl?.dirty || formControl?.touched))) return false;
+      if (formControl?.errors?.['required']) return true;
+      if (formControl?.errors?.['minlength']) return true;
+      return false;
+    }
+    isSuccessValid(formControlName: string) {
+      let formControl = this.loginForm.get(formControlName);
+      return formControl?.valid && (formControl?.dirty || formControl?.touched);
+    }
 }
