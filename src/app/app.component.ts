2c54f6ae50c5ad7fd1bde863cfb3bc0c7f7bcdda
dilayuluerguven <uluerguven245@gmail.com>
2025-12-22 15:51:28 +0300

    add RxJS Observable creation and subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PageserviceService } from './pages/pageservice.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularapp';
-  constructor(private pageservice:PageserviceService){
+  constructor(private pageservice: PageserviceService) {
     this.pageservice.write();
-
   }
-
-  save(input:string)
-  {
+  ngOnInit(): void {
+    let myobservable = new Observable<string>((data) => {
+      data.next('ahmet');
+      data.next("mehmet");
+      data.complete();
+    });
+    myobservable.subscribe(x=>{
+      console.log(x);
+    })
+  }
+  save(input: string) {
     console.log(input);
   }
 }
